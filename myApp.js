bc67feb2695abd0ff108dc9bf900990cb9b4e225
Emmanuel Calistus <Emmanuelcailstus5@gmail.com>
2025-03-25 09:07:36 -0700

    Installed Helmet.js 3.21.3 and updated myApp.js

--- myApp.js
+++ myApp.js
@@ -3,7 +3,9 @@ const helmet = require('helmet'); // Require Helmet
 
 const app = express();
 
-// Use Helmet for security
+// Use Helmet for security (older versions require this syntax)
 app.use(helmet());
 
+// Export the app
 module.exports = app;
+
