2c1a59f670bc4971be03b57bfdde5e93d7fd372a
Emmanuel Calistus <Emmanuelcailstus5@gmail.com>
2025-03-25 08:42:04 -0700

    Added Helmet security middleware

--- myApp.js
+++ myApp.js
@@ -1,19 +1,9 @@
 const express = require('express');
+const helmet = require('helmet'); // Require Helmet
+
 const app = express();
-const helmet = require("helmet");
 
-// Middleware setup
-app.use(helmet()); //  Use helmet function
-app.use(express.json()); // Enable JSON parsing
-app.use(express.urlencoded({ extended: true })); // Enable URL-encoded data parsing
+// Use Helmet for security
+app.use(helmet());
 
-// to check if the server is running
-app.get("/", (req, res) => {
-    res.send("Hello, your server is running!");
-});
-
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
