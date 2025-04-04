52bf582ac534e59cc2baedc6df3e8e6b588b8a4a
Emmanuel Calistus <Emmanuelcailstus5@gmail.com>
2025-04-04 05:11:25 -0700

    Updated myapp.js and read.md with new changes

--- myApp.js
+++ myApp.js
@@ -1,16 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
-
 const app = express();
-app.use(helmet());
 
-// Use Render's assigned port or default to 3000
-const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello, Render is working!');
-});
+const PORT = process.env.PORT || 3030;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`ECOTECH started on port ${PORT}`);
 });
