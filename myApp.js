961e7650ca01606c7e98f0dad49b00698f4a06c4
Emmanuel Calistus <Emmanuelcailstus5@gmail.com>
2025-04-04 05:59:23 -0700

    Updated myapp.js and with new changes

--- myApp.js
+++ myApp.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
+const bcrypt = require('bcrypt');
 
-const PORT = process.env.PORT || 3030;
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", 'trusted-cdn.com'],
+    }
+  },
+  noCache: true
+}))
 
-app.listen(PORT, () => {
-  console.log(`ECOTECH started on port ${PORT}`);
+
+
+
+
+
+
+
+
+
+module.exports = app;
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`ECOTECH Info Security App Started on Port ${port}`);
 });
